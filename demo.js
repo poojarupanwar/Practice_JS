63d0a7e841d00e83967ee5ef356afe9752dd3d70
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-23 23:20:46 +0530

    Delete demo.js
    Updated name is checkVowels.js

--- demo.js
+++
@@ -1,9 +0,0 @@
-let str="pooja";
-for(let i=0;i<str.length-1;i++){
-    if(str[i]=='a' || str[i]=='e' ||str[i]=='i' || str[i]=='o' ||str[i]=='u'){
-        console.log(str[i]+" -this is vowel")
-
-    }
-    
-}
-console.log(str[0]);
