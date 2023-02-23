0867bdfc2664e95bdde3d638ddfe67d83fc5db4c
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-23 23:20:04 +0530

    Create countSentenceNum.js

---
+++ myPracticeProblems/countSentenceNum.js
@@ -0,0 +1,9 @@
+function sentence(newstr){
+    let count=0;
+for(let i=newstr.length-1;i>0;i--)
+{
+    count++;
+}
+console.log(`The chars in ${newstr} word is ${count}`);
+}
+sentence("sentence");
